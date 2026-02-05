3eaa1093356d60fa086013a48ecb2e5a5f10d4d7
Conor Goold <cmgoold@protonmail.com>
2026-02-05 10:06:02 +0000

    ruff lint/format

--- assets/posts/crime-demograhpics-causality/code/main.py
+++ assets/posts/crime-demograhpics-causality/code/main.py
@@ -3,8 +3,9 @@ import pymc as pm
 
 rng = np.random.default_rng(1234)
 
+
 def ilogit(x):
-  return np.exp(x) / (1 + np.exp(x))
+    return np.exp(x) / (1 + np.exp(x))
 
 
 def common_causes():
@@ -19,42 +20,45 @@ def common_causes():
     crime = rng.binomial(1, ilogit(-1 + sex_z + -0.5 * age_z))
 
     with pm.Model() as model:
-      alpha = pm.Normal("alpha", 0, 10)
-      beta = pm.Normal("beta", 0, 2)
-      eta = pm.math.invlogit(alpha + beta * (immigration - 0.5))
-      likelihood = pm.Binomial("crime", n=1, p=eta, observed=crime)
-      idata = pm.sample(1000, tune=1000, random_seed=rng)
+        alpha = pm.Normal("alpha", 0, 10)
+        beta = pm.Normal("beta", 0, 2)
+        eta = pm.math.invlogit(alpha + beta * (immigration - 0.5))
+        likelihood = pm.Binomial("crime", n=1, p=eta, observed=crime)
+        idata = pm.sample(1000, tune=1000, random_seed=rng)
 
-    print("Wrong: ", (
+    print(
+        "Wrong: ",
         (
-            idata.posterior.beta.mean(),
-            pm.stats.hdi(idata.posterior.beta, 0.95)
+            (idata.posterior.beta.mean(), pm.stats.hdi(idata.posterior.beta, 0.95)),
+            (
+                np.exp(idata.posterior.beta).mean(),
+                pm.stats.hdi(np.exp(idata.posterior.beta), 0.95),
+            ),
         ),
-        (
-            np.exp(idata.posterior.beta).mean(),
-            pm.stats.hdi(np.exp(idata.posterior.beta), 0.95)
-        ),
-    ))
+    )
 
     with pm.Model() as model:
-      alpha = pm.Normal("alpha", 0, 10)
-      beta0 = pm.Normal("beta0", 0, 2)
-      beta1 = pm.Normal("beta1", 0, 2)
-      beta2 = pm.Normal("beta2", 0, 2)
-      eta = pm.math.invlogit(alpha + beta0 * (immigration - 0.5) + beta1 * sex_z + beta2 * age_z)
-      likelihood = pm.Binomial("crime", n=1, p=eta, observed=crime)
-      idata = pm.sample(1000, tune=1000, random_seed=rng)
+        alpha = pm.Normal("alpha", 0, 10)
+        beta0 = pm.Normal("beta0", 0, 2)
+        beta1 = pm.Normal("beta1", 0, 2)
+        beta2 = pm.Normal("beta2", 0, 2)
+        eta = pm.math.invlogit(
+            alpha + beta0 * (immigration - 0.5) + beta1 * sex_z + beta2 * age_z
+        )
+        likelihood = pm.Binomial("crime", n=1, p=eta, observed=crime)
+        idata = pm.sample(1000, tune=1000, random_seed=rng)
 
-    print("Right: ", (
+    print(
+        "Right: ",
         (
-            idata.posterior.beta0.mean(),
-            pm.stats.hdi(idata.posterior.beta0, 0.95)
+            (idata.posterior.beta0.mean(), pm.stats.hdi(idata.posterior.beta0, 0.95)),
+            (
+                np.exp(idata.posterior.beta0).mean(),
+                pm.stats.hdi(np.exp(idata.posterior.beta0), 0.95),
+            ),
         ),
-        (
-            np.exp(idata.posterior.beta0).mean(),
-            pm.stats.hdi(np.exp(idata.posterior.beta0), 0.95)
-        ),
-    ))
+    )
+
 
 def collider_bias():
     N = 5000
@@ -67,63 +71,45 @@ def collider_bias():
     crime_in_prison = crime[prison == 1]
 
     with pm.Model() as model:
-      alpha = pm.Normal("alpha", 0, 10)
-      beta = pm.Normal("beta", 0, 2)
-      eta = pm.math.invlogit(
-        alpha + 
-        beta * (immigration_in_prison - 0.5)
-      )
-      likelihood = pm.Binomial(
-        "crime", 
-        n=1, 
-        p=eta, 
-        observed=crime_in_prison
-      )
-      idata = pm.sample(1000, tune=1000, random_seed=rng)
+        alpha = pm.Normal("alpha", 0, 10)
+        beta = pm.Normal("beta", 0, 2)
+        eta = pm.math.invlogit(alpha + beta * (immigration_in_prison - 0.5))
+        likelihood = pm.Binomial("crime", n=1, p=eta, observed=crime_in_prison)
+        idata = pm.sample(1000, tune=1000, random_seed=rng)
 
-    print("Wrong: ", (
+    print(
+        "Wrong: ",
         (
-            idata.posterior.beta.mean(),
-            pm.stats.hdi(idata.posterior.beta, 0.95)
+            (idata.posterior.beta.mean(), pm.stats.hdi(idata.posterior.beta, 0.95)),
+            (
+                (1 - np.exp(idata.posterior.beta)).mean(),
+                pm.stats.hdi(1 - np.exp(idata.posterior.beta), 0.95),
+            ),
         ),
-        (
-            (1 - np.exp(idata.posterior.beta)).mean(),
-            pm.stats.hdi(1 - np.exp(idata.posterior.beta), 0.95)
-        ),
-    ))
+    )
 
     with pm.Model() as model:
-      alpha = pm.Normal("alpha", 0, 10)
-      beta = pm.Normal("beta", 0, 2)
-      eta = pm.math.invlogit(
-        alpha + 
-        beta * (immigration - 0.5)
-      )
-      likelihood = pm.Binomial(
-        "crime", 
-        n=1, 
-        p=eta, 
-        observed=crime
-      )
-      idata = pm.sample(1000, tune=1000, random_seed=rng)
+        alpha = pm.Normal("alpha", 0, 10)
+        beta = pm.Normal("beta", 0, 2)
+        eta = pm.math.invlogit(alpha + beta * (immigration - 0.5))
+        likelihood = pm.Binomial("crime", n=1, p=eta, observed=crime)
+        idata = pm.sample(1000, tune=1000, random_seed=rng)
 
-    print("Right: ", (
+    print(
+        "Right: ",
         (
-            idata.posterior.beta.mean(),
-            pm.stats.hdi(idata.posterior.beta, 0.95)
+            (idata.posterior.beta.mean(), pm.stats.hdi(idata.posterior.beta, 0.95)),
+            (
+                np.exp(idata.posterior.beta).mean(),
+                pm.stats.hdi(np.exp(idata.posterior.beta), 0.95),
+            ),
         ),
-        (
-            np.exp(idata.posterior.beta).mean(),
-            pm.stats.hdi(np.exp(idata.posterior.beta), 0.95)
-        ),
-    ))
-
+    )
 
 
 def main():
     common_causes()
     collider_bias()
-
 
 
 if __name__ == "__main__":
